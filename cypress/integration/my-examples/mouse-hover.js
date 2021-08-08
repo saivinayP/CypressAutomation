fc6587478db58d45350d94cb7dbc885a4f816e50
Sai Vinay Paladugu <paladugusaivinay@gmail.com>
2021-08-08 22:29:18 +0530

    added web table handling

---
+++ cypress/integration/my-examples/mouse-hover.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Handling mouse hover events', function () {
+    it('Launch practise page', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
+    })
+
+    it('mouse hover', function(){
+    
+    })
+})
--- cypress/integration/my-examples/web-tables.js
+++ cypress/integration/my-examples/web-tables.js
@@ -2,10 +2,18 @@
 
 describe('Handling web tables', function () {
     it('Launch practise page', function () {
-        cy.visit('https://rahulshettyacademy.com/AutomationPractice/#/')
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice')
     })
 
-    it('web tables', function(){
-    
+    it('get price of a cource from web table', function(){
+        cy.get('tr td:nth-child(2)').each(($el, index, $list) => {
+            if($el.text().includes('Python')){
+                cy.get('tr td:nth-child(2)').eq(index).next().then(function(price){
+                    const priceText = price.text()
+                    cy.log('Price of the cource is : '+priceText)
+                    expect(priceText).to.equal('25')
+                })
+            }
+        })
     })
 })
