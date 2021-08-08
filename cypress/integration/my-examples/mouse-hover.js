df019957b0e7041e7b0c4650f584f817138ec16f
Sai Vinay Paladugu <paladugusaivinay@gmail.com>
2021-08-09 00:38:54 +0530

    mouse-hover method added

--- cypress/integration/my-examples/mouse-hover.js
+++ cypress/integration/my-examples/mouse-hover.js
@@ -6,6 +6,15 @@ describe('Handling mouse hover events', function () {
     })
 
     it('mouse hover', function(){
-    
+        //show method should be applied on immediate parent of hidden element
+        cy.get('.mouse-hover-content').invoke('show')
+        cy.contains('Top').click()
+        cy.url().should('include','top')
     })
-})
+
+    it('click hidden elements', function(){
+        //click method has ability to clcik even hidden elements
+        cy.contains('Top').click({force:true})
+        cy.url().should('include','top')
+    })
+})  
